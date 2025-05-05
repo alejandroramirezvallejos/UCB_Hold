306ab3d1f9528e9e21f6bcd14276c370d3ec0eaa
josue-balbontin <158528708+josue-balbontin@users.noreply.github.com>
2025-05-04 20:24:03 -0400

    Delete IMT_Reservas directory

--- IMT_Reservas/imt_reservas.client/src/app/models/carrito.ts
+++
@@ -1,9 +0,0 @@
-export interface Carrito {
-  [id: number]: {
-    nombre: string;
-    cantidad: number;
-    fecha_inicio: Date | null;
-    fecha_final: Date | null;
-    imagen : string; 
-  };
-}
--- IMT_Reservas/imt_reservas.client/src/app/models/grupo_equipo.ts
+++
@@ -1,9 +0,0 @@
-export interface GrupoEquipo {
-  id: number;
-  nombre: string;
-  descripcion?: string;
-  modelo?: string;
-  url_data_sheet?: string;
-  marca?: string;
-  link?: string;
-}
