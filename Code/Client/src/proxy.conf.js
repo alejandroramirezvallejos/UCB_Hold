0b836a98ec49ae0885475ea2c98ee1d9920a7c0b
josue-balbontin <balbontinjosue@gmail.com>
2026-05-25 22:36:16 -0400

    subiendo conf del proxy

--- Code/Client/src/proxy.conf.js
+++ Code/Client/src/proxy.conf.js
@@ -1,6 +1,6 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:5190` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5190';
 
 const PROXY_CONFIG = [
