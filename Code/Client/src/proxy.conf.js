3ebf7ad0371e1294bf58ac0bbb75f0e96e1fd3fd
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2026-06-30 21:39:48 -0400

    chore: add prettier and proxy

--- Code/Client/src/proxy.conf.js
+++ Code/Client/src/proxy.conf.js
@@ -1,17 +1,17 @@
-const { env } = require('process');
+const { env } = require("process");
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:5190` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:5190';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:5190`
+  : env.ASPNETCORE_URLS
+    ? env.ASPNETCORE_URLS.split(";")[0]
+    : "http://localhost:5190";
 
 const PROXY_CONFIG = [
   {
-    context: [
-      "/IMT_Reservas",
-      "/api"
-    ],
+    context: ["/IMT_Reservas", "/api"],
     target,
-    secure: false
-  }
-]
+    secure: false,
+  },
+];
 
 module.exports = PROXY_CONFIG;
